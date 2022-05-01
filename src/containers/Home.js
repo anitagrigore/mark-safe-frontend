a4e5f63151d54c48ce40a35de23cc6936d930196
Anita Grigore <anita.grigore1@gmail.com>
2022-05-01 20:15:16 +0300

    Create home and login pages

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>MarkSafe</h1>
+                <p className="text-muted">See if your family is safe. With us.</p>
+            </div>
+        </div>
+    );
+}
