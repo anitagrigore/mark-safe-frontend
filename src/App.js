43c303854e320b625c67cf4798b180ec65efeefd
Anita Grigore <anita.grigore1@gmail.com>
2022-05-08 16:54:30 +0300

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, {useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
+import {Form, FormControl, Button} from "react-bootstrap";
 import "./App.css";
 import logo from "./logo1.svg"
 import Routes from "./Routes";
 import Nav from "react-bootstrap/Nav";
 import { AppContext } from "./lib/contextLib";
 import {useNavigate} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
     const navigate = useNavigate();
-    const [isAuthenticated,setAuthenticated] = useState(false);
+    const [isAuthenticated, setAuthenticated] = useState(false);
 
     function handleLogout() {
         setAuthenticated(false);
         navigate("/login");
+    }
+
+    function handleSearchInput(event) {
+        this.setState({
+            searchText: event.target.value
+        });
     }
 
     return (
@@ -30,6 +40,28 @@ function App() {
             MarkSafe
           </Navbar.Brand>
           <Navbar.Toggle />
+            <Form inline="true">
+                {isAuthenticated ? (
+                <Container>
+                    <Row>
+                        <Col lg={8}>
+                            <FormControl
+                                onChange={handleSearchInput}
+                                value=""
+                                type="text"
+                                placeholder="Search"
+                                className="mr-sm-2"
+                            />
+                        </Col>
+                        <Col>
+                            <Button bsPrefix="btn-custom" variant="primary" type="submit">
+                                Search
+                            </Button>
+                        </Col>
+                    </Row>
+                </Container>
+                ) : (<></>)}
+            </Form>
             <Navbar.Collapse className="justify-content-end">
                 {isAuthenticated ? (
                     <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
